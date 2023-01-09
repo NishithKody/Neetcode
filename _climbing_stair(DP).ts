4da7ececb693983ac74f00203998a34b358487d8
NishithKody <113385563+NishithKody@users.noreply.github.com>
2023-01-09 00:24:57 -0500

    Create _climbing_stair(DP).ts
    https://leetcode.com/problems/climbing-stairs/submissions/874513446/

---
+++ _climbing_stair(DP).ts
@@ -0,0 +1,17 @@
+function climbStairs(n: number): number {
+
+    if(n == 1 || n == 2)
+        return n
+
+    let dp = new Array(n+1).fill(0)
+    dp[1] = 1;
+    dp[2] = 2;
+
+    for(let i =3; i<=n; i++){
+        dp[i]=dp[i-1] + dp[i-2]
+        console.log(dp)
+    }
+
+    return dp[n]
+    
+};
