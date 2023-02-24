96dd8d82a763fc15fd2b484a7f048336a860ca44
NishithKody <113385563+NishithKody@users.noreply.github.com>
2023-02-23 23:22:48 -0500

    Create Length_of_last_word.js
    https://leetcode.com/problems/length-of-last-word/submissions/903923656/

---
+++ Length_of_last_word.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+
+    s=s.trim()
+    let n = s.length
+
+    for(let i =n; i>=0; i--){
+        if(s[i]==' '){
+            return n -i-1
+        }
+    }
+
+    return n
+    
+};
