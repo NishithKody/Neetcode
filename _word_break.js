58599020317f1a8eb30b7333fa15d82195da0012
NishithKody <113385563+NishithKody@users.noreply.github.com>
2023-02-24 00:12:35 -0500

    Create _word_break.js
    https://leetcode.com/problems/word-break/description/

---
+++ _word_break.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {boolean}
+ */
+var wordBreak = function(s, wordDict) {
+
+    let dp = new Array(s.length+1).fill(false)
+    dp[0]= true
+    
+    for(let i=0; i<s.length; i++){
+        if(dp[i]==true){
+            for(let word of wordDict){
+                let prefix = s.slice(i,i+word.length)
+
+                if(prefix == word){
+                    dp[i+word.length]=true
+                }
+            }
+        }
+    }
+
+    return dp[s.length]
+    
+};
