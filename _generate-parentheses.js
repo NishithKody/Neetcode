6ae3fb9434c187f29a4c543f8c550780f28b5794
NishithKody <113385563+NishithKody@users.noreply.github.com>
2023-04-23 16:28:36 -0400

    Create _generate-parentheses.js
    https://leetcode.com/problems/generate-parentheses/submissions/938623862/

---
+++ _generate-parentheses.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+
+    let res = []
+    let stack = []
+
+    let backtrack = (open, close)=>{
+        if(open==n && close==n){
+            res.push(stack.join(''))
+            return
+        }
+
+        if(open<n){
+            stack.push('(')
+            backtrack(open+1,close)
+            stack.pop()
+        }
+
+        if(close<open){
+            stack.push(')')
+            backtrack(open,close+1)
+            stack.pop()
+        }
+    }
+
+    backtrack(0,0)
+
+    return res
+    
+};
